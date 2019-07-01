b910ef2f2c09ab97587088b82267cf4fea9b752c
Sherri Cui <sherrii.cui@pm.me>
2019-07-02 10:14:05 +1200

    gradient modal working

--- components/Block.js
+++ components/Block.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Link from 'next/link'
 import { Renderer } from './Renderer'
 
@@ -25,7 +26,7 @@ const Gallery = props => {
 	let GalleryEl = props.images.map(item => {
 		return (
 			<div className="dib w-70 mr3">
-				<img src={item.fields.file.url} alt="" className="br4" />
+				<GalleryImage {...item} />
 			</div>
 		)
 	})
@@ -35,6 +36,37 @@ const Gallery = props => {
 				whiteSpace: 'nowrap'
 			}}>
 			{GalleryEl}
+		</div>
+	)
+}
+
+const GalleryImage = props => {
+	const [active, toggleActive] = useState(false)
+
+	return (
+		<div className={active ? "fixed w-100 vh-100 top-0 left-0 z-999" : "h-75vw w-100 relative"}>
+		<style jsx>{`
+			.modal::before {
+				content: "";
+				display: ${active ? 'block' : 'none'};
+				position: absolute;
+				left: 0;
+				top: -23%;
+				width: 100%;
+				height: 25%;
+				background: black;
+				background: linear-gradient(to top, rgba(0,0,0,1)25%, rgba(0,0,0,0)100%);
+			}
+		`}</style>
+			<a className="absolute top-0 left-0 w-100 h-100" 
+				onClick={(e) => {e.preventDefault(); toggleActive(!active)}}></a>
+			<img src={props.fields.file.url} alt="" className={"object " + (active ? "h-50" : "h-100 br4")} />
+			{ active &&
+				<div className="modal bg-black h-50 pa4 relative">
+					<h2 className="ma0">{props.fields.title}</h2>
+					<p>{props.fields.description}</p>
+				</div>
+			}
 		</div>
 	)
 }
