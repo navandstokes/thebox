b5c7742e463fa9e8e813680ef6842105d1c822f4
Sherri Cui <sherrii.cui@pm.me>
2019-07-15 09:03:05 +1200

    added pixel contact track

--- components/Circle.js
+++ components/Circle.js
@@ -4,7 +4,7 @@ export const Circle = props => {
 	return (
 		<div className="w3 h3 br-100 bg-dark-pink fixed bottom-2 right-2"
 			style={{boxShadow: ' 0px 7px 19px -12px rgb(0,0,0,1)'}}>
-			<a href="tel:+642041805803" className="db w-100 h-100 flex justify-center items-center">
+			<a href="tel:+642041805803" className="db w-100 h-100 flex justify-center items-center" onClick={(e) => {ReactPixel.track('Contact')}}>
 				<Icon icon="PhoneIcon" size="2rem" className="white" /> 
 			</a>
 		</div>
