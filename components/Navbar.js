f882782e7044d0a39586e094ce3692f38c7dacbd
Sherri Cui <sherrii.cui@pm.me>
2019-07-15 18:18:59 +1200

    navbar zindex

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,7 @@ import { Logo } from './Logo'
 
 export const Navbar = props => {
 	return (
-		<div className={"w-100 flex flex-row justify-between items-center gutters-pad bg-blue " + (props.nav ? 'fixed top-0 left-0 bg-white bb b--black-10' : '')}
+		<div className={"w-100 flex flex-row justify-between items-center gutters-pad bg-blue " + (props.nav ? 'fixed top-0 left-0 bg-white bb b--black-10 z-9999' : '')}
 			style={{boxShadow: '0 3px 5px 0 rgba(0,0,0,0.2)'}}>
 			<Logo />
 			<div className="flex flex-row">
