1ecae5f3978ab7d331f874e18fbf8175492a8586
Sherri Cui <sherrii.cui@pm.me>
2020-07-16 18:54:26 +0200

    moved tokens to env

--- api.js
+++ api.js
@@ -1,8 +1,8 @@
 import { createClient } from 'contentful'
 
 const api = createClient({
-	space: `wuqkqlman2bm`,
-	accessToken: `9H4Jok0vo7RR1B2uFSKiQr9uBNBqtNB-RPs9s3QvW4k`
+	space: process.env.CONTENTFUL_SPACE,
+	accessToken: process.env.CONTENTFUL_DELIVERY
 })
 
 export default api
