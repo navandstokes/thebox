66e52cbc7f2dd6f71ca95651f5ba7ac71868d6d3
Sherri Cui <sherrii.cui@pm.me>
2020-07-16 17:53:24 +0200

    pretty links

--- components/Renderer.js
+++ components/Renderer.js
@@ -15,12 +15,56 @@ export const Renderer = props => {
 				    		className="w-100 h-100 object"
 				    	/>
 		    		</div>
-		    )}
+		    )},
+		    [INLINES.HYPERLINK]: (node) => {
+		    	return (
+		    		<Squiggle href={node.data.target}>
+			    		{node.content[0].value}
+		    		</Squiggle>
+	    		)
+		    }
 		} 
 	}
 	return (
 		<div>
 		{documentToReactComponents(props.content, options)}
 		</div>
+	)
+}
+
+const Squiggle = ({ children, href }) => {
+	return (
+		<>
+		<style jsx>
+			{`
+				a {
+					position: relative;
+					z-index: 1;
+					display: inline-flex;
+					padding-left: .15rem;
+					padding-bottom: .05rem;
+					padding-right: .15rem;
+					font-size: 0.9rem;
+					font-weight: 600;
+				}
+				a::before {
+					content: "";
+					width: 100%;
+					height: 100%;
+					background-image: linear-gradient(to top, #fed330 30%, rgba(0, 0, 0, 0) 45%);
+					position: absolute;
+					left: 0;
+					bottom: 0;
+					z-index: -1;
+					will-change: width;
+					transform-origin: left bottom
+				}
+				a:hover::before {
+					width: 50%;
+				}
+			`}
+		</style>
+		<a href={href}>{ children }</a>
+		</>
 	)
 }
